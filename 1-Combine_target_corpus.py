91b32f339ef514899eed3649b66670900398bed3
Nopphawan <nopphawan.nnsw@gmail.com>
2022-04-17 10:49:46 +0700

    revise partipant 1-3

--- 1-Combine_target_corpus.py
+++ 1-Combine_target_corpus.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 
-path = "Dataset\For participant 4-16"
+path = "Dataset\For participant 1"
 
 df_main = pd.DataFrame()
 for corpus in os.listdir(path):
@@ -10,4 +10,4 @@ for corpus in os.listdir(path):
     df["corpus"] = corpus.replace(".csv","")
     df_main = pd.concat([df_main, df], ignore_index=True)
 
-df_main.to_csv("Dataset/Target-4.csv", index=None)
+df_main.to_csv("Dataset/Target-1.csv", index=None)
